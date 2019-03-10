a92ba5f0446442ff6ec3fa3e36a7f4ee70ef69d8
Jarod Peachey <jwpeachey107@aol.com>
2019-03-10 12:16:14 -0400

    Updates

--- session.js
+++ session.js
@@ -1,8 +1,11 @@
+const sessionUIControl = (function() {
+
+})()
+
 const sessionControl = (function(){
    return {
       startNewSession: function(user) {
          console.log(`A new session has been started for ${user.name}`);
-         window.location.href = 'session.html';
       }
    }
 })()
--- signup.js
+++ signup.js
@@ -1,32 +1,6 @@
-const signUpUIControl = (function () {
-
-   return {
-      displayLoginPage: function () {
-         const container = document.getElementById('container');
-
-         container.innerHTML = `
-         <form style='max-width: 500px; margin: 0 auto;'>
-            <h1 class="center-text">Log In</h1>
-            <div class="input-element medium">
-               <label for="">Username</label>
-               <input type="text" class="full-width loginUsername" placeholder="Username" id="loginUsername">
-            </div>
-            <div class="input-element medium">
-               <label for="">Password</label>
-               <input type="password" class="full-width loginPassword" placeholder="Password" id="loginPassword">
-            </div>
-            <input type="submit" value="Log In" class="accent medium full-width loginBtn" id="loginBtn">
-         </form>
-         `;
-      }
-   }
-})()
-
 const signupControl = (function (storageControl, mainDataControl) {
    const loadEventListeners = function () {
-      document.getElementById('signupBtn').addEventListener('click', signUp);
-
-      document.addEventListener('click', logIn);
+         document.getElementById('signupBtn').addEventListener('click', signUp);  
    }
 
    const signUp = function (e) {
@@ -42,8 +16,8 @@ const signupControl = (function (storageControl, mainDataControl) {
          newUser = mainDataControl.createUser(name, username, password);
 
          storageControl.addNewUser(newUser);
-         
-         signUpUIControl.displayLoginPage();
+
+         window.location.href = 'login.html';
       } else {
          users.forEach(function (user) {
             if (user.username == username) {
@@ -52,8 +26,8 @@ const signupControl = (function (storageControl, mainDataControl) {
                newUser = mainDataControl.createUser(name, username, password);
 
                storageControl.addNewUser(newUser);
-               
-               signUpUIControl.displayLoginPage();
+
+               window.location.href = 'login.html';
             }
          })
       }
@@ -61,29 +35,8 @@ const signupControl = (function (storageControl, mainDataControl) {
       e.preventDefault();
    }
 
-   const logIn = function(e) {
-      if (e.target.classList.contains('loginBtn')) {
-         let username = document.getElementById('loginUsername').value;
-         let password = document.getElementById('loginPassword').value;
-
-         console.log(username);
-         const users = storageControl.getUsers();
-
-         users.forEach(function(user) {
-            if (user.username == username && user.password == password) {
-               sessionControl.startNewSession(user);
-            } else {
-               alert('The username/password is incorrect. Please try again.');
-            }
-         })
-      }
-
-      e.preventDefault()
-   };
-
    return {
       init: function () {
-         console.log('App initialized');
          loadEventListeners();
       }
    }
