482e017b91cfcb754065c365d1043cc0dbb4984d
Jarod Peachey <jwpeachey107@aol.com>
2019-03-18 12:51:37 -0400

    Update workout BTN

--- session.js
+++ session.js
@@ -12,7 +12,7 @@ const sessionUIControl = (function () {
          }
 
          if (user.caloriesLost == 0) {
-            workoutButton = `You don't have any workouts yet. <br><br><a href="workouts.html" class="button accent">Add Meal</a>`;
+            workoutButton = `You don't have any workouts yet. <br><br><a href="workouts.html" class="button accent">Add Workout</a>`;
          }
 
          if (user.netCalories <= 0) {
